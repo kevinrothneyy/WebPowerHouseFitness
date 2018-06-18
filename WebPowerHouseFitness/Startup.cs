15c109bd816f8acccc03ef04dca1a5dfae324d36
kevinrothneyy <kevinrothneyy@gmail.com>
2018-06-18 16:19:37 -0400

    hola

--- WebPowerHouseFitness/Startup.cs
+++ WebPowerHouseFitness/Startup.cs
@@ -26,12 +26,12 @@ namespace WebPowerHouseFitness
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            /*services.AddDbContext<ApplicationDbContext>(options =>
+            services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
-                .AddDefaultTokenProviders();*/
+                .AddDefaultTokenProviders();
 
             // Add application services.
             services.AddTransient<IEmailSender, EmailSender>();
